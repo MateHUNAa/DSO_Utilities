93f6f0d06d89f2e57233e8d86aadc0d7bf8d7795
MateHUNAa <matehunter123@gmail.com>
2025-10-02 14:20:51 +0200

    Add GlobalHotkey class for handling system-wide hotkeys
    A new `GlobalHotkey.cs` file has been added to the project, and it is now included in the `DSO_Utilities.csproj` file. The `GlobalHotkey` class provides functionality for registering and handling global hotkeys in a Windows Forms application.
    
    Key features of the `GlobalHotkey` class:
    - Uses `user32.dll` to register and unregister hotkeys at the system level.
    - Includes a constructor for hotkey registration, a `ProcessMessage` method to handle `WM_HOTKEY` messages, and a `Dispose` method for cleanup.
    - Defines a `Pressed` event to allow external code to respond to hotkey presses.
    
    This addition enhances the application's ability to handle global hotkeys efficiently.

---
+++ Hotkeys/GlobalHotkey.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSO_Utilities.Hotkeys
+{
+    public class GlobalHotkey
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotkey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotkey(IntPtr hwnd, int id);
+
+        private readonly int id;
+        private readonly IntPtr handle;
+
+        public Keys key { get;  }
+        public event Action Pressed;
+
+        public GlobalHotkey(IntPtr handle, int id, Keys key)
+        {
+            this.handle = handle;
+            this.id = id;
+            this.key = key;
+
+            RegisterHotkey(handle, id, 0, (uint)key);
+        }
+
+        public void ProcessMessage(Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == id)
+            {
+                Pressed?.Invoke();
+            }
+        }
+
+        public void Dispose()
+        {
+            UnregisterHotkey(handle, id);
+        }
+    }
+}
